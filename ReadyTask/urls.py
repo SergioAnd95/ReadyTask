e97bd3a7d0ed61f0ac1b9ec6cc323f8e4acc0e6b
SergioAnd18 <developinc@yandex.ru>
2017-05-12 21:18:01 +0300

    add name to index page

--- ReadyTask/urls.py
+++ ReadyTask/urls.py
@@ -22,7 +22,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^tasks/', include('tasks.urls')),
     url(r'^basket/', include('basket.urls', namespace='basket'))
 ]
